9e0d0bd4046f5c28715e8c86e8c7ce3746cbaa90
ANU7MADHAV <anumadhavan888@gmail.com>
2024-12-07 11:43:32 +0530

    Feat : added rest api for register and login

--- user-management/cmd/api/healthcheck.go
+++ user-management/cmd/api/healthcheck.go
@@ -3,5 +3,5 @@ package main
 import "github.com/gin-gonic/gin"
 
 func (app *application) healthCheck(c *gin.Context) {
-	c.JSON(200, gin.H{"Port": app.config.port, "version": version})
+	c.JSON(200, gin.H{"status": "available", "Port": app.config.port, "version": version})
 }
