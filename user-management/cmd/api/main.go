e8b53241c25388f4ec5d541ee2c6144e982b4923
ANU7MADHAV <anumadhavan888@gmail.com>
2024-12-07 11:16:48 +0530

    Feat: added healcheck api

--- user-management/cmd/api/main.go
+++ user-management/cmd/api/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+type config struct {
+	port int
+}
+
+type application struct {
+	config config
+	logger *log.Logger
+}
+
+const version = "1.23"
 
 func main() {
-	fmt.Println("hell world")
+	var config config
+
+	flag.IntVar(&config.port, "port", 8080, "Application starting port")
+	flag.Parse()
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	app := &application{
+		config: config,
+		logger: logger,
+	}
+
+	srv := http.Server{
+		Addr:         fmt.Sprintf(":%d", config.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	err := srv.ListenAndServe()
+	log.Fatal(err)
 }
